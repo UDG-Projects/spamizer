9acd22eeef39e52e67d7bb29e8cf383e6c1665d8
Francesc <fxbullich@gmail.com>
2019-05-05 22:20:15 +0200

    [FEAT] Validador implementat per una colecció de mails. Falta implementar el naive bayes method

--- src/spamizer/MLCore/NaiveBayes.java
+++ src/spamizer/MLCore/NaiveBayes.java
@@ -8,12 +8,14 @@ import java.util.Collection;
 public class NaiveBayes implements Method {
 
     @Override
-    public ProbabylityResult getProbabilities(Database database, Collection<String> words) {
-        ProbabylityResult result= new ProbabylityResult();
+    public boolean isSpam(Database database, Collection<String> message, int k, int phi) {
+        double spamProbability=0;
+        double hamProbability=0;
 
 
 
 
-        return result;
+        //comparem les probabilitats obtingudes aplicant el parametre phi.
+        return spamProbability > (hamProbability*phi);
     }
 }
--- src/spamizer/interfaces/Method.java
+++ src/spamizer/interfaces/Method.java
@@ -7,5 +7,12 @@ import java.util.Collection;
 
 public interface Method {
 
-    ProbabylityResult getProbabilities(Database database, Collection<String> words);
+    /**
+     *
+     * @param database
+     * @param message Collection of strings of the message to be calssificated
+     * @param k
+     * @return true if message is classificated as spam
+     */
+    boolean isSpam(Database database, Collection<String> message, int k, int phi);
 }
